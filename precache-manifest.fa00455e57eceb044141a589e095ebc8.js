63ec71d1ef33e1cde07bb481fdf5622217fa49a4
waseemahmad01 <wa031522@gmail.com>
2020-08-19 10:14:23 +0500

    Updates

---
+++ precache-manifest.fa00455e57eceb044141a589e095ebc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c96c1637c63536ce6367d8fd820b1012",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "680973d3edcee64f2457",
+    "url": "/calculator/static/css/main.92c964cc.chunk.css"
+  },
+  {
+    "revision": "95c78c726ee364012a6e",
+    "url": "/calculator/static/js/2.a2e0f6bb.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/calculator/static/js/2.a2e0f6bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "680973d3edcee64f2457",
+    "url": "/calculator/static/js/main.156bf715.chunk.js"
+  },
+  {
+    "revision": "2f771d3db25210da76fb",
+    "url": "/calculator/static/js/runtime-main.47932f8d.js"
+  }
+]);
